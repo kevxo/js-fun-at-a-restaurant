9756aa3a88dab133239f88ab507ffa2f5408dc90
Kevin Cuadros <kev040798@gmail.com>
2021-05-16 18:58:57 -0400

    Finish Iteration 2 Order

--- src/order.js
+++ src/order.js
@@ -1,7 +1,34 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    deliveryOrders.push(order);
+  }
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNumber) {
+      deliveryOrders.splice(i, 1);
+    }
+  }
+}
+
+function listItems(deliveryOrders) {
+  return deliveryOrders.map(order => order.item).join(', ')
+}
+
+function searchOrder(deliveryOrders, item) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === item){
+      return true;
+    }
+  }
+
+  return false;
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
