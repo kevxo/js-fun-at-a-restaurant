a3c9f2bcc8f618bedf8961810aefca9021da39cf
Kevin Cuadros <kev040798@gmail.com>
2021-05-18 16:47:43 -0400

    Finish extension

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,30 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+
+  greetCustomer(name, morning = false) {
+    if (morning === true) {
+      return `Good morning, ${name}!`
+    }
+
+    return `Hello, ${name}!`
+  }
+
+  checkForFood(foodItem){
+    var menus = this.restaurant.menus
+    for (const [key, value] of Object.entries(menus)){
+       if (key === foodItem.type){
+        for (var i = 0; i < value.length; i++) {
+          if (value[i].name === foodItem.name){
+            return `Yes, we're serving ${foodItem.name} today!`
+          }
+        }
+      }
+    }
+
+    return `Sorry, we aren't serving ${foodItem.name} today.`
   }
 }
 
